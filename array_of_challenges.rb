b69197d278d3f7c92b835d563d49db823ab8f5c1
Jamie Pilgrim <hello@jamiepilgrim.com>
2016-02-02 11:21:02 -0600

    added donut dibs

---
+++ array_of_challenges.rb
@@ -0,0 +1,24 @@
+#Line up Alphabetically 
+	#Sort all the names in this array alphabetically
+	star_students = [ "Kelly", "Henry", "Theo", "Jennifer", "Michelle"]
+
+puts star_students[2]
+	#create a new array that has 19 elements of only your name as a string
+		#(hint: There is a way do this with only typing the string once)
+
+
+	#That first array, star_students and this array, brilliant_students, should be one big array!
+		#combine this array with the one above
+		#Then sort the final array in alphabetic order
+
+	brilliant_students = ["Vedant", "Lena", "Zara", "Jessica"]
+
+	
+	#BONUS
+		#Iterate through the final student array, printing each student with an adjective
+			#ex:   Dynamic Dean   /n  Witty Andrew  /n  Diplomatic Luke
+
+		#Extra Bonus: Match the adjective with the student by index
+
+			adjectives = ["adventurous", "ambitious", "brave", "bright", "charming", "courageous", "creative", "determined", "dynamic", "diplomatic", "enthusiastic", "helpful", "honest", "humorous", "inventive", "persistent", "powerful", "reliable", "resourceful", "sincere", "thoughtful", "witty"]
+
---
+++ donut_dibs.rb
@@ -0,0 +1,22 @@
+donut_box1 = ["Boston Creme", "Boston Creme", "Boston Creme", "Choc Long John", "Choc Long John", "Vanilla Long John", "Vanilla Long John", "Old Fashioned", "Old Fashioned", "Old Fashioned", "French thing", "French Thing"]
+donut_box2 = ["Choc Sprinkles", "Choc Sprinkles", "double choc cake", "double choc cake", "double choc cake", "glazed", "glazed", "glazed", "choc cake", "choc cake", "choc cake"]
+
+
+#There are two boxes of delicious donuts sitting in front of you. They're everyrthing you dreamed of and more. 
+#But there's a problem. We don't know which donut belongs to which one of your classmates!
+
+
+
+#Create a hash where donuts are assigned to specific classmates
+
+
+#Return the donut of a specific person
+
+
+#Find three hash methods and experiment with them here:
+
+
+
+#BONUS: Iterate through the hash and print a sentence for each student/donut pair. Example: "James calls dibs on a chocolate cake donut!"
+
+
